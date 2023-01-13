cebce4e33c989cc71222c82b4f06e37ff7a6c222
Sohaib-GO <s.almajmaie@gmail.com>
2023-01-12 17:40:00 -0700

    Add Helper function to get schedule for a particular day

---
+++ src/helpers/selectors.js
@@ -0,0 +1,10 @@
+export function getAppointmentsForDay(state, day) {
+  const filteredDays = state.days.filter((days) => days.name === day);
+  if (filteredDays.length === 0) {
+    return [];
+  }
+  const filteredAppointments = filteredDays[0].appointments.map(
+    (appointment) => state.appointments[appointment]
+  );
+  return filteredAppointments;
+}
