37cabe71affaf4b2c1cdcc11a18e6a5a624e7dd2
Sohaib-GO <s.almajmaie@gmail.com>
2023-01-16 18:14:57 -0700

    Create New Appointments
    - Create New Appointments and show them to the user
    - Insert the new created appointment into the backend so they're not deleted after a refresh
    - Show saving indicator after creating a new appointment

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,16 +5,32 @@ import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
 import Form from "components/Appointment/Form";
 import useVisualMode from "hooks/useVisualMode";
+import Status from "components/Appointment/Status";
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
+const SAVING = "SAVING";
 
 export default function Appointment(props) {
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
 
-  
-return (
+  function save(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer,
+    }
+    // call saving indicator
+    transition(SAVING)
+    props.bookInterview(props.id, interview)
+
+      .then(() => {
+        transition(SHOW)
+      })
+
+  }
+
+  return (
     <article className="appointment">
       <Header time={props.time} />
       {mode === EMPTY && <Empty onAdd={() => transition("CREATE")} />}
@@ -24,15 +40,14 @@ return (
           interviewer={props.interview.interviewer}
           onEdit={() => transition("EDIT")}
           onDelete={() => transition("CONFIRM")}
-
         />
       )}
       {mode === "CREATE" && (
-        <Form
-          interviewers={props.interviewers}
-          onCancel={back}
-        />
+        <Form interviewers={props.interviewers} onCancel={back} onSave={save} />
       )}
+
+      {mode === SAVING && <Status message="Saving" />}
+
     </article>
   );
 }
